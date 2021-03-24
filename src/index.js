6760aa796ab514482618fbd3fa5bc139c6c24722
eddietindame <eddie.tindame@googlemail.com>
2021-03-24 19:36:28 +0000

    Scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import './index.css'
+import './index.scss'
 import { Board } from './components/board'
 import configureStore from './store'
 import { Provider } from 'react-redux'
